c907cf27b705847c04669fe545a9bd0d99f42297
rosenhristov <rosen.hristov@dataart.com>
2022-02-24 14:11:51 +0200

    Initial commit: upload application

---
+++ modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/impl/GuestbookLocalServiceImpl.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.docs.guestbook.service.impl;
+
+import com.liferay.docs.guestbook.exception.GuestbookNameException;
+import com.liferay.docs.guestbook.model.Guestbook;
+import com.liferay.docs.guestbook.service.base.GuestbookLocalServiceBaseImpl;
+import com.liferay.headless.commerce.admin.account.dto.v1_0.User;
+import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Date;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Rosen Hristov
+ */
+@Component(
+	property = "model.class.name=com.liferay.docs.guestbook.model.Guestbook",
+	service = AopService.class
+)
+public class GuestbookLocalServiceImpl extends GuestbookLocalServiceBaseImpl {
+	
+	public Guestbook addGuestbook(long userId, String name, ServiceContext serviceContext) 
+			throws PortalException {
+		User user = (User) userLocalService.getUserById(userId);
+		Date now = new Date();
+		
+		validate(name);
+		
+		long guestbookId = counterLocalService.increment();
+
+		Guestbook guestbook = guestbookPersistence.create(guestbookId);
+		
+		guestbook.setUuid(serviceContext.getUuid());
+		guestbook.setUserId(userId);
+		guestbook.setGroupId(serviceContext.getScopeGroupId());
+		guestbook.setCompanyId(user.getCompanyId());
+		guestbook.setUserName(user.getFullName());
+		guestbook.setCreateDate(serviceContext.getCreateDate(now));
+		guestbook.setModifiedDate(serviceContext.getModifiedDate(now));
+		guestbook.setName(name);
+		guestbook.setExpandoBridgeAttributes(serviceContext);
+
+		guestbookPersistence.update(guestbook);
+
+		return guestbook;
+	}
+	
+	protected void validate(String name) throws PortalException {
+		if (Validator.isNull(name)) {
+			throw new GuestbookNameException();
+		}
+	}
+}
